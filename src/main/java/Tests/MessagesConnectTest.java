a61b171bb9850c379ad302b8b30dde72783690de
kupra <lkupr21@freeuni.edu.ge>
2023-07-10 01:36:16 +0400

    MessagesConnect tests

--- src/main/java/Tests/MessagesConnectTest.java
+++ src/main/java/Tests/MessagesConnectTest.java
@@ -5,18 +5,16 @@ import static org.junit.jupiter.api.Assertions.*;
 import FunctionalClasses.MessagesConnect;
 import FunctionalClasses.UserConnect;
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.w3c.dom.Text;
 
 import java.sql.SQLException;
 
 public class MessagesConnectTest {
     private static MessagesConnect mConnect;
-    private static UserConnect uConnect;
 
     @BeforeAll
     public static void setup() {
+        UserConnect uConnect;
         mConnect = new MessagesConnect(true);
         uConnect = new UserConnect(true);
         uConnect.addUser("1", "1");
@@ -55,25 +53,25 @@ public class MessagesConnectTest {
     @Test
     public void test3() throws SQLException {
 
-        mConnect.sendMessage(5,6, "1");
+        mConnect.sendMessage(5, 6, "1");
         mConnect.sendMessage(6, 5, "2");
-        assertEquals("1", mConnect.getMessagesWith(5,6).get(0).getMessage());
-        assertEquals("2", mConnect.getMessagesWith(5,6).get(1).getMessage());
+        assertEquals("1", mConnect.getMessagesWith(5, 6).get(0).getMessage());
+        assertEquals("2", mConnect.getMessagesWith(5, 6).get(1).getMessage());
     }
 
     //same test as "test4" but sent 2--->1 and then 1-->2 (beforeEach) ******************
     @Test
-    public void test4() throws SQLException{
-        mConnect.sendMessage(6,5, "1");
+    public void test4() throws SQLException {
+        mConnect.sendMessage(6, 5, "1");
         mConnect.sendMessage(5, 6, "2");
-        assertEquals("1", mConnect.getMessagesWith(5,6).get(0).getMessage());
-        assertEquals("2", mConnect.getMessagesWith(5,6).get(1).getMessage());
+        assertEquals("1", mConnect.getMessagesWith(5, 6).get(0).getMessage());
+        assertEquals("2", mConnect.getMessagesWith(5, 6).get(1).getMessage());
     }
 
 
     //Mixed messages
     @Test
-    public void test6() throws SQLException{
+    public void test6() throws SQLException {
         mConnect.sendMessage(7, 9, "79");
         mConnect.sendMessage(7, 10, "710");
         mConnect.sendMessage(10, 7, "107");
@@ -82,12 +80,12 @@ public class MessagesConnectTest {
         assertEquals(2, mConnect.getMessagesWith(7, 10).size());
         assertEquals(1, mConnect.getMessagesWith(8, 9).size());
         assertEquals(0, mConnect.getMessagesWith(8, 7).size());
-        assertEquals("79", mConnect.getMessagesWith(7,9).get(0).getMessage());
-        assertEquals("710", mConnect.getMessagesWith(7,10).get(0).getMessage());
-        assertEquals("107", mConnect.getMessagesWith(7,10).get(1).getMessage());
-        assertEquals("98", mConnect.getMessagesWith(8,9).get(0).getMessage());
-        assertEquals("98", mConnect.getMessagesWith(9,8).get(0).getMessage());
-        assertEquals("810", mConnect.getMessagesWith(8,10).get(0).getMessage());
+        assertEquals("79", mConnect.getMessagesWith(7, 9).get(0).getMessage());
+        assertEquals("710", mConnect.getMessagesWith(7, 10).get(0).getMessage());
+        assertEquals("107", mConnect.getMessagesWith(7, 10).get(1).getMessage());
+        assertEquals("98", mConnect.getMessagesWith(8, 9).get(0).getMessage());
+        assertEquals("98", mConnect.getMessagesWith(9, 8).get(0).getMessage());
+        assertEquals("810", mConnect.getMessagesWith(8, 10).get(0).getMessage());
 
     }
 
