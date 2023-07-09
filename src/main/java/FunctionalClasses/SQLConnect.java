97aac3a90e36898f8d38e20fb709297e40f03145
agurg <agurg21@freeuni.edu.ge>
2023-07-09 22:08:09 +0400

    Fix multiple queries execute

--- src/main/java/FunctionalClasses/SQLConnect.java
+++ src/main/java/FunctionalClasses/SQLConnect.java
@@ -3,11 +3,7 @@ package FunctionalClasses;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 
 public class SQLConnect {
     protected final String database = "QUIZ_WEBSITE";
@@ -23,6 +19,7 @@ public class SQLConnect {
         } else {
             server += database;
         }
+        server += "?allowMultiQueries=true";
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
             connect = DriverManager.getConnection
@@ -35,7 +32,6 @@ public class SQLConnect {
     public void createAgain() throws IOException, SQLException {
         String s = System.getProperty("user.dir") + "/src/main/java/SqlScripts/SqlScriptTEST.sql";
         String query = Files.readString(Path.of(s));
-        System.out.println(query);
         Statement stmt = connect.createStatement();
         stmt.execute(query);
     }
