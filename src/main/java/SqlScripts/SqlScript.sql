95d36fc643d5f1cbc7c368e6c52d497f452bf899
kupra <lkupr21@freeuni.edu.ge>
2023-07-11 14:15:41 +0400

    Code refactoring

--- src/main/java/SqlScripts/SqlScript.sql
+++ src/main/java/SqlScripts/SqlScript.sql
@@ -17,24 +17,24 @@ CREATE TABLE USERS
 CREATE TABLE FRIENDS
 (
     ID       int primary key NOT NULL AUTO_INCREMENT,
-    USER1ID  int             not null,
-    USER2ID  int             not null,
+    USER1_ID  int             not null,
+    USER2_ID  int             not null,
     ACCEPTED tinyint default 0,
     CHECK (ACCEPTED = 0 OR ACCEPTED = 1),
-    FOREIGN KEY (USER1ID) REFERENCES USERS (ID),
-    FOREIGN KEY (USER2ID) REFERENCES USERS (ID)
+    FOREIGN KEY (USER1_ID) REFERENCES USERS (ID),
+    FOREIGN KEY (USER2_ID) REFERENCES USERS (ID)
 );
 
 CREATE TABLE QUIZZES
 (
     ID           int primary key NOT NULL AUTO_INCREMENT,
-    QUIZNAME     CHAR(64) UNIQUE not null,
-    DESCRIPT     TEXT,
+    QUIZ_NAME     CHAR(64) UNIQUE not null,
+    DESCRIPTION     TEXT,
     COMPLETED    INT      default 0,
-    CREATIONTIME DATETIME default current_timestamp,
-    CREATORID    INT             not null,
+    CREATION_TIME DATETIME default current_timestamp,
+    CREATOR_ID    INT             not null,
     QUESTIONS    CHAR(64),
-    FOREIGN KEY (CREATORID) REFERENCES USERS (ID)
+    FOREIGN KEY (CREATOR_ID) REFERENCES USERS (ID)
 );
 
 CREATE TABLE COMPLETED_QUIZZES
@@ -52,21 +52,21 @@ CREATE TABLE COMPLETED_QUIZZES
 CREATE TABLE CHALLENGES
 (
     ID      int primary key NOT NULL AUTO_INCREMENT,
-    USER1ID int             not null,
-    USER2ID int             not null,
-    QUIZID  INT             not null,
-    FOREIGN KEY (USER1ID) REFERENCES USERS (ID),
-    FOREIGN KEY (USER2ID) REFERENCES USERS (ID),
-    FOREIGN KEY (QUIZID) references QUIZZES (ID)
+    USER1_ID int             not null,
+    USER2_ID int             not null,
+    QUIZ_ID  INT             not null,
+    FOREIGN KEY (USER1_ID) REFERENCES USERS (ID),
+    FOREIGN KEY (USER2_ID) REFERENCES USERS (ID),
+    FOREIGN KEY (QUIZ_ID) references QUIZZES (ID)
 );
 
 CREATE TABLE MESSAGES
 (
     ID       int primary key NOT NULL AUTO_INCREMENT,
-    USER1ID  int             not null,
-    USER2ID  int             not null,
+    USER1_ID  int             not null,
+    USER2_ID  int             not null,
     MESSAGE  TEXT,
-    SENDDATE DATETIME default current_timestamp,
-    FOREIGN KEY (USER1ID) REFERENCES USERS (ID),
-    FOREIGN KEY (USER2ID) REFERENCES USERS (ID)
+    SEND_DATE DATETIME default current_timestamp,
+    FOREIGN KEY (USER1_ID) REFERENCES USERS (ID),
+    FOREIGN KEY (USER2_ID) REFERENCES USERS (ID)
 );
