e11af8baea99f1e3a1ea67049c276495214c29de
kupra <lkupr21@freeuni.edu.ge>
2023-07-22 01:34:12 +0400

    Question classes final

--- src/main/java/Types/QuestionAbstract.java
+++ src/main/java/Types/QuestionAbstract.java
@@ -5,12 +5,16 @@ import java.util.ArrayList;
 public abstract class QuestionAbstract implements Question {
     private final String questionText;
     private final ArrayList<String> answers;
+    private final String type;
 
-    public QuestionAbstract(String questionText, ArrayList<String> answers) {
+    public QuestionAbstract(String type, String questionText, ArrayList<String> answers) {
         this.questionText = questionText;
         this.answers = answers;
+        this.type = type;
     }
-
+    public String getType(){
+        return type;
+    }
     @Override
     public ArrayList<String> getAnswers() {
         return answers;
