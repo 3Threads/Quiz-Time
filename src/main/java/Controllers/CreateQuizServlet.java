ed6a1fd167b5b419eda50256a5e9c64cf57f9155
agurg <agurg21@freeuni.edu.ge>
2023-07-22 17:37:53 +0400

    Get ready for back end

--- src/main/java/Controllers/CreateQuizServlet.java
+++ src/main/java/Controllers/CreateQuizServlet.java
@@ -6,6 +6,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Arrays;
 
 @WebServlet(name = "createQuiz", value = "/createQuiz")
 public class CreateQuizServlet extends HttpServlet {
@@ -20,10 +21,11 @@ public class CreateQuizServlet extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) throws IOException, ServletException {
-        if (httpServletRequest.getSession().getAttribute("userInfo") == null) {
-            httpServletResponse.sendRedirect("/login");
-        } else {
-            httpServletRequest.getRequestDispatcher("createQuiz.jsp").forward(httpServletRequest, httpServletResponse);
+        if (httpServletRequest.getParameter("action") != null && httpServletRequest.getParameter("action").equals("addQuestion")) {
+            if(httpServletRequest.getParameter("questionType").equals("questionResponse")){
+                String questionText= httpServletRequest.getParameter("questionText");
+                String[] answers = httpServletRequest.getParameterValues("answer");
+            }
         }
     }
 }
