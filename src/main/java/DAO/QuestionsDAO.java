f3552f830f56b7dc3122cd5e1d387f78485220c4
kupra <lkupr21@freeuni.edu.ge>
2023-07-22 18:14:58 +0400

    Added questionsDAO

---
+++ src/main/java/DAO/QuestionsDAO.java
@@ -0,0 +1,99 @@
+package DAO;
+
+import BusinessLogic.ListToString;
+import Types.Question;
+import Types.QuestionResponse;
+import Types.Quiz;
+import org.apache.commons.dbcp2.BasicDataSource;
+
+import java.sql.*;
+import java.util.ArrayList;
+
+public class QuestionsDAO {
+    private final BasicDataSource dataSource;
+
+    public QuestionsDAO(BasicDataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+    public void addQuestion(String questionText, int quizId, String type, ArrayList<String> answers) {
+        Question question = new QuestionResponse(questionText, type, answers);
+        Connection connect = null;
+        try {
+            connect = dataSource.getConnection();
+            String str = "INSERT INTO QUESTIONS VALUES(default, ?, ?, ?, ?)";
+            PreparedStatement statement = connect.prepareStatement(str);
+            String categoryName = question.getType();
+            statement.setString(1, categoryName);
+            statement.setInt(2, quizId);
+            statement.setString(3, questionText);
+            ListToString lts = new ListToString();
+            String res = lts.generateString(answers);
+            statement.setString(4, res);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            if (connect != null) {
+                try {
+                    connect.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    public void removeQuestion(int questionId) {
+        Connection connect = null;
+        try {
+            connect = dataSource.getConnection();
+            String str = "DELETE FROM QUESTIONS WHERE ID = ?;";
+            PreparedStatement statement = connect.prepareStatement(str);
+            statement.setInt(1, questionId);
+            statement.execute();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            if (connect != null) {
+                try {
+                    connect.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    public ArrayList<Integer> getQuestionsIdByQuizId(int quizId) {
+        Connection connect = null;
+        try {
+            connect = dataSource.getConnection();
+            String str = "SELECT * FROM QUESTIONS WHERE ID = ?;";
+            PreparedStatement statement = connect.prepareStatement(str);
+            statement.setInt(1, quizId);
+            return getQuestions(statement);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            if (connect != null) {
+                try {
+                    connect.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return null;
+    }
+
+    private ArrayList<Integer> getQuestions(PreparedStatement statement) throws SQLException {
+        ArrayList<Integer> questions = new ArrayList<>();
+        ResultSet result = statement.executeQuery();
+        while (result.next()) {
+            int questionId = result.getInt("ID");
+            questions.add(questionId);
+        }
+        return questions;
+    }
+}
+
