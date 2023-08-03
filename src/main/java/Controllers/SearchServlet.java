bf6361888e09e1763a7bb0a50aec9f786340aff2
m1ch1n1o <nkhet21@freeuni.edu.ge>
2023-08-03 21:43:19 +0400

    added search page

--- src/main/java/Controllers/SearchServlet.java
+++ src/main/java/Controllers/SearchServlet.java
@@ -3,6 +3,7 @@ package Controllers;
 import BusinessLogic.SessionRemove;
 import DAO.UsersDAO;
 
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -14,23 +15,11 @@ import java.io.IOException;
 public class SearchServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) throws IOException {
-        if (!SessionRemove.checkUser(httpServletRequest)) {
-            httpServletResponse.sendRedirect("/login");
-            return;
+        try {
+            httpServletRequest.getRequestDispatcher("searchPage.jsp").forward(httpServletRequest, httpServletResponse);
+        } catch (ServletException e) {
+            throw new RuntimeException(e);
         }
-        SessionRemove.removeQuizAttributes(httpServletRequest);
-        String searchUserName = httpServletRequest.getParameter("search");
-        if(searchUserName==null || searchUserName.trim().equals("")){
-            httpServletResponse.sendRedirect("/homePage");
-            return;
-        }
-        UsersDAO usConnect = (UsersDAO) httpServletRequest.getServletContext().getAttribute("usersDB");
-        int searchUserId;
-        searchUserId = usConnect.getUserId(searchUserName);
-        if (searchUserId != -1) {
-            httpServletResponse.sendRedirect("/profile?user=" + searchUserId);
-        } else httpServletResponse.sendRedirect("/homePage");
-
     }
 
     @Override
