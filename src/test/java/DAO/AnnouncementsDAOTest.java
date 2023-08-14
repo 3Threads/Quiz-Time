2b6230c9616b2c8a0d6551bb40aad1a6408e5740
AkakiGurgenidze <agurg21@freeuni.edu.ge>
2023-08-14 15:07:13 +0400

    Test file (not finished)

--- src/test/java/DAO/AnnouncementsDAOTest.java
+++ src/test/java/DAO/AnnouncementsDAOTest.java
@@ -3,19 +3,21 @@ package DAO;
 import Types.Announcement;
 import org.apache.commons.dbcp2.BasicDataSource;
 import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
-import java.sql.SQLException;
 import java.util.ArrayList;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class AnnouncementsDAOTest {
 
     private static AnnouncementsDAO announcementsDAO;
     private static BasicDataSource dataSource;
-    private static final String[] tableNames = new String[]{"USERS", "ANNOUNCEMENTS"};
+    private static final String[] TABLE_NAMES = new String[]{"ANNOUNCEMENTS", "USERS"};
+    private static final int USERS_NUM = 3;
+    private static final int[] ADMINS_ID = new int[USERS_NUM];
 
     @BeforeAll
     public static void setup() {
@@ -24,25 +26,26 @@ class AnnouncementsDAOTest {
         announcementsDAO = new AnnouncementsDAO(dataSource);
         UsersDAO usersDAO = new UsersDAO(dataSource);
 
-        usersDAO.addUser("ADMIN1", "ADMIN");
-        usersDAO.addUser("ADMIN2", "ADMIN");
-        usersDAO.addUser("ADMIN3", "ADMIN");
-        usersDAO.getAdminToUser(1);
-        usersDAO.getAdminToUser(2);
-        usersDAO.getAdminToUser(3);
+        for (int i = 1; i <= USERS_NUM; i++) {
+            usersDAO.addUser("ADMIN" + i, "ADMIN");
+            ADMINS_ID[i - 1] = usersDAO.getUserId("ADMIN" + i);
+            int x= usersDAO.getUserId("ADMIN" + i);
+            System.out.println(x);
+            usersDAO.getAdminToUser(ADMINS_ID[i - 1]);
+        }
     }
 
     @Test
     void testGetAnnouncements() {
-        announcementsDAO.addAnnouncement("title0", "body0", 1);
-        announcementsDAO.addAnnouncement("title1", "body1", 2);
-        announcementsDAO.addAnnouncement("title2", "body2", 3);
+        announcementsDAO.addAnnouncement("title0", "body0", ADMINS_ID[0]);
+        announcementsDAO.addAnnouncement("title1", "body1", ADMINS_ID[0]);
+        announcementsDAO.addAnnouncement("title2", "body2", ADMINS_ID[0]);
 
         ArrayList<Announcement> announcements = announcementsDAO.getAnnouncements();
         for (int i = 0; i < announcements.size(); i++) {
             assertEquals("title" + i, announcements.get(i).getTitle());
             assertEquals("body" + i, announcements.get(i).getBody());
-            assertEquals(i + 1, announcements.get(i).getWriterId());
+            assertEquals(ADMINS_ID[0], announcements.get(i).getWriterId());
         }
 
     }
@@ -50,20 +53,25 @@ class AnnouncementsDAOTest {
     @Test
     void testAddAnnouncement() {
         ArrayList<Announcement> announcements = announcementsDAO.getAnnouncements();
-        assertEquals(3, announcements.size());
+        assertEquals(0, announcements.size());
         for (int i = 0; i < 25; i++) {
-            announcementsDAO.addAnnouncement("title" + i, "body" + i, 1);
+            announcementsDAO.addAnnouncement("title" + i, "body" + i, ADMINS_ID[0]);
         }
         announcements = announcementsDAO.getAnnouncements();
-        assertEquals(28, announcements.size());
+        assertEquals(25, announcements.size());
     }
 
     @Test
     void searchAnnouncement() {
     }
 
+    @AfterEach
+    public void finishUnitTest() {
+        DataSource.clearTables(dataSource, new String[]{TABLE_NAMES[0]});
+    }
+
     @AfterAll
-    public static void finish() throws SQLException {
-        DataSource.clearTables(dataSource, tableNames);
+    public static void finish() {
+        DataSource.clearTables(dataSource, TABLE_NAMES);
     }
 }
