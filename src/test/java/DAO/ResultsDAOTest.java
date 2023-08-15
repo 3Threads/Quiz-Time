4c1a8a8a9c28f09623c8b69794f835d50fa6a503
m1ch1n1o <nkhet21@freeuni.edu.ge>
2023-08-15 18:40:21 +0400

    Refactored Results DAO

--- src/test/java/DAO/ResultsDAOTest.java
+++ src/test/java/DAO/ResultsDAOTest.java
@@ -20,7 +20,7 @@ public class ResultsDAOTest {
     private static final int USERS_NUM = 5;
     private static final int[] USERS_ID = new int[USERS_NUM];
     private static final int QUIZZES_NUM = 6;
-    private static final int[] QUIZ_ID = new int[QUIZZES_NUM];
+    private static final int[] QUIZZES_ID = new int[QUIZZES_NUM];
 
     /*
         Creating users and quizzes
@@ -39,9 +39,9 @@ public class ResultsDAOTest {
             USERS_ID[i - 1] = usersDAO.getUserId("USER" + i);
         }
 
-        for (int i = 1; i <= 6; i++) {
+        for (int i = 1; i <= QUIZZES_NUM; i++) {
             quizzesDAO.addQuiz("QZ" + i, "new quiz", USERS_ID[0], null, "Sports");
-            QUIZ_ID[i - 1] = quizzesDAO.getQuizByName("QZ" + i).getQuizId();
+            QUIZZES_ID[i - 1] = quizzesDAO.getQuizByName("QZ" + i).getQuizId();
         }
 
     }
@@ -51,24 +51,24 @@ public class ResultsDAOTest {
      */
     @Test
     public void testAddResult() {
-        resultsDAO.addResult(USERS_ID[2], QUIZ_ID[3], 10, 2000);
-        ArrayList<Result> res = resultsDAO.getUserResultsOnQuiz(USERS_ID[2], QUIZ_ID[3]);
+        resultsDAO.addResult(USERS_ID[2], QUIZZES_ID[3], 10, 2000);
+        ArrayList<Result> res = resultsDAO.getUserResultsOnQuiz(USERS_ID[2], QUIZZES_ID[3]);
         assertEquals(1, res.size());
         assertEquals(10, res.get(0).getScore());
 
-        resultsDAO.addResult(USERS_ID[3], QUIZ_ID[5], 30, 3000);
-        res = resultsDAO.getUserResultsOnQuiz(USERS_ID[3], QUIZ_ID[5]);
+        resultsDAO.addResult(USERS_ID[3], QUIZZES_ID[5], 30, 3000);
+        res = resultsDAO.getUserResultsOnQuiz(USERS_ID[3], QUIZZES_ID[5]);
         assertEquals(1, res.size());
         assertEquals(30, res.get(0).getScore());
 
-        resultsDAO.addResult(USERS_ID[4], QUIZ_ID[0], 14, 500);
-        res = resultsDAO.getUserResultsOnQuiz(USERS_ID[4], QUIZ_ID[0]);
+        resultsDAO.addResult(USERS_ID[4], QUIZZES_ID[0], 14, 500);
+        res = resultsDAO.getUserResultsOnQuiz(USERS_ID[4], QUIZZES_ID[0]);
         assertEquals(1, res.size());
         assertEquals(14, res.get(0).getScore());
 
-        resultsDAO.addResult(USERS_ID[0], QUIZ_ID[0], 10, 1000);
-        resultsDAO.addResult(USERS_ID[0], QUIZ_ID[0], 15, 800);
-        res = resultsDAO.getUserResultsOnQuiz(USERS_ID[0], QUIZ_ID[0]);
+        resultsDAO.addResult(USERS_ID[0], QUIZZES_ID[0], 10, 1000);
+        resultsDAO.addResult(USERS_ID[0], QUIZZES_ID[0], 15, 800);
+        res = resultsDAO.getUserResultsOnQuiz(USERS_ID[0], QUIZZES_ID[0]);
         assertEquals(2, res.size());
         assertEquals(15, res.get(0).getScore());
         assertEquals(10, res.get(1).getScore());
@@ -79,18 +79,18 @@ public class ResultsDAOTest {
      */
     @Test
     public void testGetQuizResults() {
-        resultsDAO.addResult(USERS_ID[0], QUIZ_ID[1], 40, 900);
-        ArrayList<Result> res = resultsDAO.getQuizResults(QUIZ_ID[1]);
+        resultsDAO.addResult(USERS_ID[0], QUIZZES_ID[1], 40, 900);
+        ArrayList<Result> res = resultsDAO.getQuizResults(QUIZZES_ID[1]);
         assertEquals(1, res.size());
-        assertEquals(QUIZ_ID[1], res.get(0).getQuizId());
+        assertEquals(QUIZZES_ID[1], res.get(0).getQuizId());
         assertEquals(USERS_ID[0], res.get(0).getUserId());
         assertEquals(40, res.get(0).getScore());
         assertEquals(900, res.get(0).getSpentTime());
 
-        resultsDAO.addResult(USERS_ID[3], QUIZ_ID[5], 50, 120);
-        res = resultsDAO.getQuizResults(QUIZ_ID[5]);
+        resultsDAO.addResult(USERS_ID[3], QUIZZES_ID[5], 50, 120);
+        res = resultsDAO.getQuizResults(QUIZZES_ID[5]);
         assertEquals(1, res.size());
-        assertEquals(QUIZ_ID[5], res.get(0).getQuizId());
+        assertEquals(QUIZZES_ID[5], res.get(0).getQuizId());
         assertEquals(USERS_ID[3], res.get(0).getUserId());
         assertEquals(50, res.get(0).getScore());
         assertEquals(120, res.get(0).getSpentTime());
@@ -103,13 +103,13 @@ public class ResultsDAOTest {
     @Test
     public void testGerQuizResultsHard() {
         for (int i = 0; i < USERS_NUM; i++) {
-            resultsDAO.addResult(USERS_ID[i], QUIZ_ID[0], 100 * (USERS_NUM - i), 1000 * (USERS_NUM - i));
+            resultsDAO.addResult(USERS_ID[i], QUIZZES_ID[0], 100 * (USERS_NUM - i), 1000 * (USERS_NUM - i));
         }
 
-        ArrayList<Result> res = resultsDAO.getQuizResults(QUIZ_ID[0]);
+        ArrayList<Result> res = resultsDAO.getQuizResults(QUIZZES_ID[0]);
         assertEquals(USERS_NUM, res.size());
         for (int i = 0; i < USERS_NUM; i++) {
-            assertEquals(QUIZ_ID[0], res.get(i).getQuizId());
+            assertEquals(QUIZZES_ID[0], res.get(i).getQuizId());
             assertEquals(USERS_ID[i], res.get(i).getUserId());
             assertEquals(100 * (USERS_NUM - i), res.get(i).getScore());
             assertEquals(1000 * (USERS_NUM - i), res.get(i).getSpentTime());
@@ -121,19 +121,19 @@ public class ResultsDAOTest {
      */
     @Test
     public void testUserResults() {
-        resultsDAO.addResult(USERS_ID[3], QUIZ_ID[5], 50, 120);
-        resultsDAO.addResult(USERS_ID[3], QUIZ_ID[2], 100, 1200);
+        resultsDAO.addResult(USERS_ID[3], QUIZZES_ID[5], 50, 120);
+        resultsDAO.addResult(USERS_ID[3], QUIZZES_ID[2], 100, 1200);
 
         ArrayList<Result> res = resultsDAO.getUserResults(USERS_ID[3]);
         assertEquals(res.size(), 2);
 
         assertEquals(USERS_ID[3], res.get(0).getUserId());
-        assertEquals(QUIZ_ID[2], res.get(0).getQuizId());
+        assertEquals(QUIZZES_ID[2], res.get(0).getQuizId());
         assertEquals(100, res.get(0).getScore());
         assertEquals(1200, res.get(0).getSpentTime());
 
         assertEquals(USERS_ID[3], res.get(1).getUserId());
-        assertEquals(QUIZ_ID[5], res.get(1).getQuizId());
+        assertEquals(QUIZZES_ID[5], res.get(1).getQuizId());
         assertEquals(50, res.get(1).getScore());
         assertEquals(120, res.get(1).getSpentTime());
 
@@ -141,25 +141,25 @@ public class ResultsDAOTest {
         /*
             If score is same it should be sorted by spentTime
          */
-        resultsDAO.addResult(USERS_ID[0], QUIZ_ID[5], 100, 1000);
-        resultsDAO.addResult(USERS_ID[0], QUIZ_ID[2], 100, 2000);
-        resultsDAO.addResult(USERS_ID[0], QUIZ_ID[3], 100, 3000);
+        resultsDAO.addResult(USERS_ID[0], QUIZZES_ID[5], 100, 1000);
+        resultsDAO.addResult(USERS_ID[0], QUIZZES_ID[2], 100, 2000);
+        resultsDAO.addResult(USERS_ID[0], QUIZZES_ID[3], 100, 3000);
 
         res = resultsDAO.getUserResults(USERS_ID[0]);
         assertEquals(res.size(), 3);
 
         assertEquals(USERS_ID[0], res.get(0).getUserId());
-        assertEquals(QUIZ_ID[5], res.get(0).getQuizId());
+        assertEquals(QUIZZES_ID[5], res.get(0).getQuizId());
         assertEquals(100, res.get(0).getScore());
         assertEquals(1000, res.get(0).getSpentTime());
 
         assertEquals(USERS_ID[0], res.get(1).getUserId());
-        assertEquals(QUIZ_ID[2], res.get(1).getQuizId());
+        assertEquals(QUIZZES_ID[2], res.get(1).getQuizId());
         assertEquals(100, res.get(1).getScore());
         assertEquals(2000, res.get(1).getSpentTime());
 
         assertEquals(USERS_ID[0], res.get(2).getUserId());
-        assertEquals(QUIZ_ID[3], res.get(2).getQuizId());
+        assertEquals(QUIZZES_ID[3], res.get(2).getQuizId());
         assertEquals(100, res.get(2).getScore());
         assertEquals(3000, res.get(2).getSpentTime());
     }
@@ -175,29 +175,29 @@ public class ResultsDAOTest {
         friendsDAO.sendFriendRequest(USERS_ID[0], USERS_ID[2]);
         friendsDAO.acceptRequest(USERS_ID[2], USERS_ID[0]);
 
-        resultsDAO.addResult(USERS_ID[1], QUIZ_ID[0], 100, 1000);
-        resultsDAO.addResult(USERS_ID[1], QUIZ_ID[1], 300, 2000);
-        resultsDAO.addResult(USERS_ID[2], QUIZ_ID[1], 200, 3000);
+        resultsDAO.addResult(USERS_ID[1], QUIZZES_ID[0], 100, 1000);
+        resultsDAO.addResult(USERS_ID[1], QUIZZES_ID[1], 300, 2000);
+        resultsDAO.addResult(USERS_ID[2], QUIZZES_ID[1], 200, 3000);
 
-        ArrayList<Result> res = resultsDAO.getUserFriendsResultOnQuiz(USERS_ID[0], QUIZ_ID[0]);
+        ArrayList<Result> res = resultsDAO.getUserFriendsResultOnQuiz(USERS_ID[0], QUIZZES_ID[0]);
 
         assertEquals(1, res.size());
         assertEquals(USERS_ID[1], res.get(0).getUserId());
-        assertEquals(QUIZ_ID[0], res.get(0).getQuizId());
+        assertEquals(QUIZZES_ID[0], res.get(0).getQuizId());
         assertEquals(100, res.get(0).getScore());
         assertEquals(1000, res.get(0).getSpentTime());
 
 
-        res = resultsDAO.getUserFriendsResultOnQuiz(USERS_ID[0], QUIZ_ID[1]);
+        res = resultsDAO.getUserFriendsResultOnQuiz(USERS_ID[0], QUIZZES_ID[1]);
 
         assertEquals(2, res.size());
         assertEquals(USERS_ID[1], res.get(0).getUserId());
-        assertEquals(QUIZ_ID[1], res.get(0).getQuizId());
+        assertEquals(QUIZZES_ID[1], res.get(0).getQuizId());
         assertEquals(300, res.get(0).getScore());
         assertEquals(2000, res.get(0).getSpentTime());
 
         assertEquals(USERS_ID[2], res.get(1).getUserId());
-        assertEquals(QUIZ_ID[1], res.get(1).getQuizId());
+        assertEquals(QUIZZES_ID[1], res.get(1).getQuizId());
         assertEquals(200, res.get(1).getScore());
         assertEquals(3000, res.get(1).getSpentTime());
     }
@@ -207,7 +207,6 @@ public class ResultsDAOTest {
     public void finishUnitTest() {
         DataSource.clearTables(dataSource, new String[]{TABLE_NAMES[0]});
     }
-
 
     @AfterAll
     public static void finish() {
